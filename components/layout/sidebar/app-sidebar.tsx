06206e76815853c2ed39f7cfa054eb90e3012726
fajar_a_prasetio_x <69068262+FajarAdi25@users.noreply.github.com>
2025-03-13 16:13:48 +0800

    Update app-sidebar.tsx

--- components/layout/sidebar/app-sidebar.tsx
+++ components/layout/sidebar/app-sidebar.tsx
@@ -28,10 +28,6 @@ const data = {
           title: "Add Address Book",
           url: "/add-address-book",
         },
-        // {
-        //   title: "Edit Address Book",
-        //   url: "/edit-address-book",
-        // },
       ],
     },
   ],
