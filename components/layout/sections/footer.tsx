026bc270dd598d77f942845ea1d647db50a160ee
fajar_a_prasetio_x <69068262+FajarAdi25@users.noreply.github.com>
2025-03-21 09:57:04 +0800

    Update footer.tsx

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -1,19 +1,63 @@
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { ChevronsDownIcon } from "lucide-react";
+import { ChevronsDownIcon, Mail, Phone } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export const FooterSection = () => {
   return (
-    <footer id="footer" className="container pt-5 pb-1">
-      <div className="p-10 bg-card border border-secondary rounded-2xl">
-        <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
-          <div className="col-span-full xl:col-span-2">
-            <Link href="/" className="flex font-bold items-center">
-              <h1 className="text-xl font-bold">Unified Golf</h1>
-            </Link>
+    <footer id="footer" className="container pt-5 mb-2">
+      <div className="p-5 bg-card border border-secondary rounded-2xl">
+        <div className="flex flex-col w-full">
+          <div className="flex flex-nowrap justify-between items-center w-full ml-1">
+            <div className="flex flex-col items-start gap-1">
+              <Link href="/" className="flex items-center">
+                <Image
+                  src={"/company-logo.jpg"}
+                  width={80}
+                  height={30}
+                  alt="Company Logo"
+                  className="rounded-md"
+                />
+              </Link>
+              <h1 className="font-bold">Unified Golf Pte Ltd</h1>
+              <span className="text-xs font-bold pb-1">
+                1, Thomson Road #04-330G, Singapore, 300001
+              </span>
+            </div>
+            <div className="h-full mt-20 flex-col gap-1">
+              <Link href={"tel:+6586929998"} className="text-xs font-bold">
+                <Button variant={"link"}>
+                  <span className="text-white flex items-center gap-2">
+                    <Phone className="h-4 w-4" />
+                    +65 8692 9998
+                  </span>
+                </Button>
+              </Link>
+              <Link href={"tel:+60125499839"} className="text-xs font-bold">
+                <Button variant={"link"}>
+                  <span className="text-white flex items-center gap-2">
+                    <Phone className="h-4 w-4" />
+                    +6012 549 9839
+                  </span>
+                </Button>
+              </Link>
+
+              <Link
+                href={"mailto:theunifiedgolf@gmail.com"}
+                className="text-xs font-bold"
+              >
+                <Button variant={"link"}>
+                  <span className="text-white flex items-center gap-2">
+                    <Mail className="h-4 w-4" />
+                    theunifiedgolf@gmail.com
+                  </span>
+                </Button>
+              </Link>
+            </div>
           </div>
 
-          <div className="flex flex-col gap-2">
+          {/* <div className="flex flex-col gap-2">
             <h3 className="font-bold text-lg">Contact</h3>
             <div>
               <Link
@@ -41,9 +85,9 @@ export const FooterSection = () => {
                 Instagram
               </Link>
             </div>
-          </div>
+          </div> */}
 
-          <div className="flex flex-col gap-2">
+          {/* <div className="flex flex-col gap-2">
             <h3 className="font-bold text-lg">Platforms</h3>
             <div>
               <Link
@@ -71,9 +115,9 @@ export const FooterSection = () => {
                 Web
               </Link>
             </div>
-          </div>
+          </div> */}
 
-          <div className="flex flex-col gap-2">
+          {/* <div className="flex flex-col gap-2">
             <h3 className="font-bold text-lg">Help</h3>
             <div>
               <Link
@@ -92,11 +136,11 @@ export const FooterSection = () => {
                 FAQ
               </Link>
             </div>
-          </div>
+          </div> */}
         </div>
 
-        <Separator className="my-6" />
-        <section className="">
+        <Separator className="my-2" />
+        <section className="flex justify-center items-center">
           <h3 className="">
             &copy; 2025 Copyright
             {/* <Link
