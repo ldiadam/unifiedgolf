34f17739ba7e6629d979b926829bdd662ea3263f
Fajar Adi Prasetio <69068262+FajarAdi25@users.noreply.github.com>
2025-04-22 16:03:39 +0800

    fix loading table for address book

--- app/(admin-panel)/admin/address-book/list/components/table/table-address-book/data-table.tsx
+++ app/(admin-panel)/admin/address-book/list/components/table/table-address-book/data-table.tsx
@@ -103,7 +103,7 @@ export function DataTable<TData, TValue>({
                     <div className="flex flex-col items-center justify-start h-full">
                       <Loader2 className="h-8 w-8 animate-spin text-primary mb-4" />
                       <p className="text-sm text-muted-foreground">
-                        Memuat data, mohon tunggu...
+                        Loading, Please Wait...
                       </p>
                     </div>
                   </TableCell>
