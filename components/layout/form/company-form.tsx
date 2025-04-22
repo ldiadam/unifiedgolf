15c493736fdd1e95bb79192e8954437f002495b0
Fajar Adi Prasetio <69068262+FajarAdi25@users.noreply.github.com>
2025-04-22 09:21:22 +0800

    update form validation for company form

--- components/layout/form/company-form.tsx
+++ components/layout/form/company-form.tsx
@@ -45,28 +45,36 @@ const formSchema = z.object({
   company_address_building: z
     .string()
     .min(1, { message: "Field must be fill" }),
-  company_street_number: z
-    .preprocess((val) => (val === "" ? undefined : Number(val)), z.number())
-    .refine((val) => !isNaN(val), { message: "Must be a valid number" }),
+  company_street_number: z.preprocess(
+    (val) => (val === "" ? null : Number(val)),
+    z.number()
+  ),
+  // .refine((val) => !isNaN(val), { message: "Must be a valid number" }),
   company_street_name: z.string().min(1, { message: "Field must be fill" }),
-  company_building_no: z
-    .preprocess((val) => (val === "" ? undefined : Number(val)), z.number())
-    .refine((val) => !isNaN(val), { message: "Must be a valid number" }),
+  company_building_no: z.preprocess(
+    (val) => (val === "" ? null : Number(val)),
+    z.number()
+  ),
+  // .refine((val) => !isNaN(val), { message: "Must be a valid number" }),
   company_building_unit: z.string().min(1, { message: "Field must be fill" }),
   company_building_name: z.string().min(1, { message: "Field must be fill" }),
   company_city: z.string().min(1, { message: "Field must be fill" }),
   company_state: z.string().min(1, { message: "Field must be fill" }),
   company_country: z.string().min(1, { message: "Field must be fill" }),
-  company_zip_code: z
-    .preprocess((val) => (val === "" ? undefined : Number(val)), z.number())
-    .refine((val) => !isNaN(val), { message: "Must be a valid number" }),
-  company_fax: z
-    .preprocess((val) => (val === "" ? undefined : Number(val)), z.number())
-    .refine((val) => !isNaN(val), { message: "Must be a valid number" }),
+  company_zip_code: z.preprocess(
+    (val) => (val === "" ? null : Number(val)),
+    z.number()
+  ),
+  // .refine((val) => !isNaN(val), { message: "Must be a valid number" }),
+  company_fax: z.preprocess(
+    (val) => (val === "" ? null : Number(val)),
+    z.number()
+  ),
+  // .refine((val) => !isNaN(val), { message: "Must be a valid number" }),
   company_website: z.string().min(1, { message: "Field must be fill" }),
   company_pic: z.string().min(1, { message: "Field must be fill" }),
   company_designation: z.string().min(1, { message: "Field must be fill" }),
-  company_email: z.string().email({ message: "Field must be email address" }),
+  company_email: z.string(),
   company_phone: z.string().min(1, { message: "Field must be fill" }),
   categories: z.array(z.number()).default([]),
 });
