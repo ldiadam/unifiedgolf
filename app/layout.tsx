6e7333acaf7e0b4899c36999a4c39a5efc58303a
Fajar Adi Prasetio <69068262+FajarAdi25@users.noreply.github.com>
2025-05-15 18:58:40 +0800

    add viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,22 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/utils/utils";
 import { ThemeProvider } from "@/components/layout/theme-provider";
-import { SearchProvider } from "@/hook/search-context";
 import { NavbarNew } from "@/components/layout/navbar-new";
 import { Toaster } from "@/components/ui/sonner";
 import { CompanyProvider } from "@/hook/company-context";
 import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported but less commonly used
+  // interactiveWidget: 'resizes-visual',
+};
 export default function RootLayout({
   children,
 }: Readonly<{
