84cb7dd5c14d8b5a24533492f6b1c4910430d85b
Fajar Adi Prasetio <69068262+FajarAdi25@users.noreply.github.com>
2025-05-26 11:22:28 +0800

    update mainpage for dekstop

--- app/(app)/(home)/page.tsx
+++ app/(app)/(home)/page.tsx
@@ -1,21 +1,33 @@
-import { FooterSection } from "@/components/layout/sections/footer";
-import Tag from "@/components/layout/tag";
-import Image from "next/image";
-
 export default function CompanyProfilePage() {
   return (
     <>
-      {/* Background Image */}
-      <div className="w-full h-auto shadow-lg -mb-34">
-        <img
-          src="/bg-home-3.png"
-          alt="Golf course background"
-          className="w-full h-auto opacity-85"
-        />
+      {/* Here is for mobile */}
+      <div className="block md:hidden">
+        <div className="w-full h-auto shadow-lg -mb-34">
+          <img
+            src="/bg-home-3.png"
+            alt="Golf course background"
+            className="w-full h-auto opacity-85"
+          />
+        </div>
+        <footer className="bg-card/80 text-white text-xs text-center py-4">
+          © 2025 Unified Golf Pte Ltd. All rights reserved.
+        </footer>
       </div>
-      <footer className="bg-card/80 text-white text-xs text-center py-4">
-        © 2025 Unified Golf Pte Ltd. All rights reserved.
-      </footer>
+
+      {/* Here is for dekstop*/}
+      <div className="hidden md:block md:h-auto">
+        <div className="w-full h-auto shadow-lg -mb-34">
+          <img
+            src="/bg-home-desktop.png"
+            alt="Golf course background"
+            className="w-full h-auto opacity-85"
+          />
+        </div>
+        <footer className="bg-card/80 text-white text-xs text-center py-4">
+          © 2025 Unified Golf Pte Ltd. All rights reserved.
+        </footer>
+      </div>
     </>
   );
 }
