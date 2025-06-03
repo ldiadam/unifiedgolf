a98d5be6c780bcf02db0336e0ae0659cf76f6b70
Rinaldi Adam <rinaldiadam@live.com>
2025-06-03 14:23:09 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,10 @@ import type { Viewport } from "next";
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported but less commonly used
+  // interactiveWidget: 'resizes-visual',
 };
 export default function RootLayout({
   children,
